04c72eede7d645e0c1d7a2437cf4f175a635fb4a
Noah Semus <33107099+noahsemus@users.noreply.github.com>
2022-01-30 09:58:23 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,91 @@
+<script>
+	import Card from './components/Card.svelte';
+	import { fly } from 'svelte/transition';
+	import { onMount } from 'svelte';
+	const contents = [
+		{
+			user: {
+				username: 'Noah Semus',
+				profilePic:
+					'https://images.pexels.com/photos/3222422/pexels-photo-3222422.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+			},
+			userReview: {
+				title: 'Pulp Fiction',
+				rating: '8.5/10',
+				review: 'Bloody and gory. Fun time.',
+				poster: 'http://cdn.shopify.com/s/files/1/0969/9128/products/Pulp_Fiction_-_Uma_Thurman_Mia_Wallace_-_Quentin_Tarantino_Hollywood_Movie_Poster_b9d2e54b-a73e-46fc-8435-fcc672c41424_grande.jpg?v=1572088239',
+			},
+		},
+		{
+			user: {
+				username: 'Jack Trego',
+				profilePic:
+					'https://images.pexels.com/photos/6507483/pexels-photo-6507483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+			},
+			userReview: {
+				title: 'The Silence of the Lambs',
+				rating: '9/10',
+				review: 'Intriguing. Classic polic procedural. Good.',
+				poster: 'https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
+			},
+		},
+		{
+			user: {
+				username: 'Jack Trego',
+				profilePic:
+					'https://images.pexels.com/photos/6507483/pexels-photo-6507483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+			},
+			userReview: {
+				title: 'The Silence of the Lambs',
+				rating: '9/10',
+				review: 'Intriguing. Classic polic procedural. Good.',
+				poster: 'https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
+			},
+		},
+	];
+
+	let ready = false;
+	onMount(() => (ready = true));
+</script>
+
+<main>
+	<div class="topBar" />
+	<div class="container">
+		{#if ready}
+			{#each contents as content, i}
+				<div transition:fly={{ y: 80, duration: 1000, delay: i * 200 }}>
+					<Card {...content} />
+				</div>
+			{/each}
+		{/if}
+	</div>
+</main>
+
+<style>
+	main {
+		background: var(--sys--dark--background);
+		width: 100vw;
+		height: 100vh;
+		overflow-y: auto;
+		overflow-x: hidden;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		gap: 16px;
+		padding: 16px;
+		overflow: hidden;
+		margin-top: 80px;
+	}
+
+	.topBar {
+		width: 100vw;
+		height: 80px;
+		background: black;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 9;
+	}
+</style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,99 @@
+<script>
+	export let user = {
+		username: 'Jack Trego',
+		profilePic:
+			'https://images.pexels.com/photos/3222422/pexels-photo-3222422.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+	};
+	export let likeCount = 0;
+	export let userReview = {
+		title: 'Pulp Fiction',
+		rating: '8.5/10',
+		review: 'Bloody and gory. Fun time.',
+		poster: 'https://images.pexels.com/photos/10705224/pexels-photo-10705224.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+	};
+</script>
+
+<div class="cardContain">
+	<div class="topBar">
+		<div class="left">
+			<img class="profilePic" src={user.profilePic} alt="User profile" />
+			<p class="titleMedium">{user.username}</p>
+		</div>
+		<div class="right">
+			<p class="titleMedium">{likeCount}</p>
+			<img class="icon" src="icons/like-empty.svg" alt="Like button" />
+		</div>
+	</div>
+	<div class="content">
+		<img class="poster" src={userReview.poster} alt="Movie poster" />
+		<div class="text">
+			<p
+				class="titleMedium"
+				style="font-weight: 800; letter-spacing: 0.5px"
+			>
+				{userReview.title}
+			</p>
+			<p class="titleMedium" style="margin-bottom: 16px;">
+				{userReview.rating}
+			</p>
+			<p class="headlineSmall">{userReview.review}</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.cardContain {
+		width: 100%;
+		border: 1px solid var(--read-only--dark--surface3);
+		border-radius: 16px;
+		overflow: hidden;
+		cursor: pointer;
+		background: var(--sys--dark--surface);
+		color: var(--sys--dark--on-surface);
+		position: relative;
+	}
+
+	.content {
+		padding: 16px;
+		padding-top: 0px;
+		display: flex;
+		gap: 16px;
+	}
+
+	.poster {
+		width: 135px;
+		height: 210px;
+		border: 1px solid var(--read-only--dark--surface3);
+		border-radius: 16px;
+	}
+
+	.profilePic {
+		width: 40px;
+		height: 40px;
+		border-radius: 20px;
+	}
+
+	.topBar {
+		padding: 16px;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.topBar .left,
+	.topBar .right {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: center;
+		gap: 8px;
+	}
+
+	.text {
+		width: max-content;
+	}
+
+	p {
+		margin: 0;
+	}
+</style>
